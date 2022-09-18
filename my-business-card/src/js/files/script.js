8e39a530169f4bfadcac6b7be761fcb48583a177
Artur <golubev.artur.1986@gmail.com>
2022-09-18 18:43:33 +0300

    feat: implement page about.html

--- my-business-card/src/js/files/script.js
+++ my-business-card/src/js/files/script.js
@@ -2,3 +2,14 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
+// ======== Показать еще карточки
+const btnshowMoreCards = document.querySelector('.button-more');
+const hiddenCards = document.querySelectorAll('.card-link--hidden');
+
+
+// ======== Клик по кнопке и показ тзуч скрытых карточек
+btnshowMoreCards.addEventListener('click', function () {
+	hiddenCards.forEach(function (card) {
+		card.classList.remove('card-link--hidden');
+	})
+});
